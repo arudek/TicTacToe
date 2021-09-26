2835727eda64d5d35d4fb42cdc8e6d8781804382
Arne Rudek <mail@arne-rudek.de>
2021-09-26 13:01:37 +0200

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Arne Rudek on 26.09.21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
